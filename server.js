4d654d1497f23c0189eb722660e819544814afe5
Faith <faiththe1rst@gmail.com>
2022-08-16 02:07:50 +0100

    in progress

--- server.js
+++ server.js
@@ -6,7 +6,14 @@ const io = require("socket.io")(http, {
 });
 const port = process.env.PORT || 4000;
 
-const users = { debs_admin: { name: "debs_admin", color: "teal" } };
+const users = {
+  debs_admin: {
+    name: "debs_admin",
+    color: "teal",
+    avatar: "/static/media/avataaars (1).66edf8ec36b7a3b93ed1.png",
+    id: ''
+  },
+};
 const names = ["debs_admin"];
 const chatHistory = [
   {
@@ -20,51 +27,54 @@ const chatHistory = [
 const sendNames = () => {
   const available = [];
   Object.values(users).map((item) => available.push(item.name));
-  io.emit("in-chat", available);
+  io.emit("in-chat", available,  Object.values(users));
 };
-io.on("connection", (socket) => {
+io.on("connection", (socket) => { 
   socket.on("username", (name) => {
-    let response;
-    if (names.includes(name.toLowerCase())) {
-      response = false;
-    } else {
-      response = true;
-      names.push(name.toLowerCase());
-      let color;
-      switch (Math.floor(Math.random() * 7 + 1)) {
-        case 1:
-          color = "teal";
-          break;
-        case 2:
-          color = "green";
-          break;
-        case 3:
-          color = "orange";
-          break;
-        case 4:
-          color = "pink";
-          break;
-        case 5:
-          color = "purple";
-          break;
-        case 6:
-          color = "blue";
-          break;
-        case 7:
-          color = "deep-blue";
-          break;
-      }
-      users[name] = { name: name, color: color, id: socket.id };
-    }
-    console.log(users);
-    io.emit("response",socket.id, response);
+    const response = names.includes(name.toLowerCase()) ? false : true;
+    io.emit("response", socket.id, response);
   });
-
-  socket.on("msgSent", (name, msg, dateSent) => { 
+  socket.on("register", (name, avatar) => {
+    names.push(name.toLowerCase());
+    let color;
+    switch (Math.floor(Math.random() * 7 + 1)) {
+      case 1:
+        color = "teal";
+        break;
+      case 2:
+        color = "green";
+        break;
+      case 3:
+        color = "orange";
+        break;
+      case 4:
+        color = "pink";
+        break;
+      case 5:
+        color = "purple";
+        break;
+      case 6:
+        color = "blue";
+        break;
+      case 7:
+        color = "deep-blue";
+        break;
+    }
+    users[name] = { name: name, color: color, id: socket.id, avatar: avatar };
+    console.log(users);
+    sendNames();
+  });
+  socket.on("msgSent", (name, msg, dateSent, avatar) => {
     let color = users[name].color;
-    chatHistory.push({ name, msg, color, senderId: users[name].id, dateSent });
+    chatHistory.push({
+      name,
+      msg,
+      color,
+      senderId: users[name].id,
+      dateSent,
+      avatar,
+    });
     io.emit("message", chatHistory);
-    // io.emit("message", name, msg, color, users[name].id, dateSent);
     sendNames();
   });
   socket.on("get-details", (id) => {
